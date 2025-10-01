28bbce021f194f886a43d2de0779b64dfbb5c730
johnedwardfry <johnedwardfry@gmail.com>
2025-10-01 10:48:55 -0700

    Redirect script output to log file
    Modified launcher.sh to redirect all stdout and stderr from main.py to translator_output.log for easier debugging and monitoring.

--- launcher.sh
+++ launcher.sh
@@ -4,6 +4,7 @@ set -e
 # Navigate to the script's directory
 cd "$(dirname "$0")"
 
-# Activate the virtual environment and run the Python script
+# Activate the virtual environment and run the Python script,
+# redirecting all output and errors to a log file.
 source .venv/bin/activate
-python3 main.py
+python3 main.py > translator_output.log 2>&1
